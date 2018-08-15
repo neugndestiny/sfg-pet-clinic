99993ded545f2e2a9f320edd68d9d0f3fddc0922
neugndestiny <neungdestiny12@gmail.com>
2018-08-15 13:35:42 +0700

    Using primary bean annotation

--- src/main/java/warakorn/springframework/sfgpetclinic/services/PrimaryThaiGreetingService.java
+++ src/main/java/warakorn/springframework/sfgpetclinic/services/PrimaryThaiGreetingService.java
@@ -1,14 +1,15 @@
 package warakorn.springframework.sfgpetclinic.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-//@Primary /*If more than one bean, I wanna use this one*/
-public class PrimaryGreetingService implements GreetingService {
-
+@Primary
+@Profile("es")
+public class PrimaryThaiGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello, this is primary greeting service";
+        return "สวัสดี";
     }
 }
